3a15a04bb3131b5e297c3cd19b312fffd9771119
Priyansh Singh <priyanshsingh855@gmail.com>
2026-05-11 20:37:35 +0530

    refactor: remove deprecated configuration utility module

--- ai-crm-hcp/frontend/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import crmReducer from './slices/crmSlice';
-
-export const store = configureStore({
-  reducer: {
-    crm: crmReducer,
-  },
-});
